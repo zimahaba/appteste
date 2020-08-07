5aca62e6e103e8fb2473d5b1fdefd833a7266e93
zimahaba <pmaquitorres@gmail.com>
2020-08-07 08:53:07 -0300

    first commit

---
+++ src/main/java/com/zimahaba/appteste/ApptesteApplication.java
@@ -0,0 +1,13 @@
+package com.zimahaba.appteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptesteApplication.class, args);
+	}
+
+}
